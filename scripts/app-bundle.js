047315871a09a7b8e4b2f4c2835ab8acdc9b974e
Eduardo Oliveira de Souza <eduardo@contabilizei.com.br>
2016-12-09 18:19:29 -0200

    Adicionando lista de contatos

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -66,6 +66,10 @@ define('resources/index',["require", "exports"], function (require, exports) {
     exports.configure = configure;
 });
 
+
+
+define("contact-list", [],function(){});
+
 define('text!app.html', ['module'], function(module) { module.exports = "<template>\n    <require from=\"bootstrap/css/bootstrap.css\"></require>\n    <require from=\"./styles.css\"></require>\n    \n    <nav class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">\n        <div class=\"navbar-header\">\n            <a class=\"navbar-brand\" href=\"#\">\n                <i class=\"fa fa-user\"></i>\n                <span>Contatos</span>\n            </a>\n        </div>\n    </nav>\n    \n    <div class=\"container\">\n        <div class=\"row\">\n            <div class=\"col-md-4\">Lista de Contatos placeholder</div>\n            <router-view class=\"col-md-8\"></router-view>\n        </div>\n    </div>\n</template>\n"; });
 define('text!styles.css', ['module'], function(module) { module.exports = "body { padding-top: 70px; }\n\nsection {\n  margin: 0 20px;\n}\n\na:focus {\n  outline: none;\n}\n\n.navbar-nav li.loader {\n    margin: 12px 24px 0 6px;\n}\n\n.no-selection {\n  margin: 20px;\n}\n\n.contact-list {\n  overflow-y: auto;\n  border: 1px solid #ddd;\n  padding: 10px;\n}\n\n.panel {\n  margin: 20px;\n}\n\n.button-bar {\n  right: 0;\n  left: 0;\n  bottom: 0;\n  border-top: 1px solid #ddd;\n  background: white;\n}\n\n.button-bar > button {\n  float: right;\n  margin: 20px;\n}\n\nli.list-group-item {\n  list-style: none;\n}\n\nli.list-group-item > a {\n  text-decoration: none;\n}\n\nli.list-group-item.active > a {\n  color: white;\n}\n"; });
 define('text!no-selection.html', ['module'], function(module) { module.exports = "<template>\r\n    <div class=\"no-selecion text-center\">\r\n        <h2>${message}</h2>\r\n    </div>\r\n</template>"; });
